afec576c799d025e340a661ef6807d5fb02b1a61
Matthew Dolan <mdolancode@gmail.com>
2023-01-30 10:21:58 -0800

    Combine. Terms and conditions.

--- Combine+UIKit2/ViewController.swift
+++ Combine+UIKit2/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var acceptedSwitch: UISwitch!
+    @IBOutlet var privacySwitch: UISwitch!
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var submitButton: UIButton!
+    
+    // Define publishers
+    @Published private var acceptedTerms = false
+    @Published private var acceptedPrivacy = false
+    @Published private var name = ""
+    
+    // Combine publishers into single stream
+    private var validToSubmit: AnyPublisher<Bool, Never> {
+        return Publishers.CombineLatest3($acceptedTerms, $acceptedPrivacy, $name)
+            .map { terms, privacy, name in
+                return terms && privacy && !name.isEmpty
+            }.eraseToAnyPublisher()
+    }
+    
+    // Define subscriber
+    private var buttonSubscriber: AnyCancellable?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameField.delegate = self
+        
+        // Hook subscriber up to publisher
+        buttonSubscriber = validToSubmit
+            .receive(on: RunLoop.main)
+            .assign(to: \.isEnabled, on: submitButton)
+     
     }
 
+    @IBAction func acceptTerms(_ sender: UISwitch) {
+        acceptedTerms = sender.isOn
+    }
+    
+    
+    @IBAction func acceptPrivacy(_ sender: UISwitch) {
+        acceptedPrivacy = sender.isOn
+    }
+    
+    
+    @IBAction func nameChanged(_ sender: UITextField) {
+        name = sender.text ?? ""
+    }
+    
+    
+    @IBAction func submitAction(_ sender: UIButton) {
+    }
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
